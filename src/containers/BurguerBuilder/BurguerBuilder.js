a35ed930f4df1debec5b2a660c92a837d62c92c7
Rai <rainieromadrid@gmail.com>
2018-03-04 12:25:07 -0500

    Creating the order sumary modal
    Signed-off-by: Rai <rainieromadrid@gmail.com>

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -80,13 +82,16 @@ class BurguerBuilder extends Component {
         }
         return (
             <Aux>
-              <Burger ingredients={this.state.ingredients} />
-              <BuildControls
-              ingredientAdded={this.addIngredientHandler}
-              ingredientRemoved={this.removeIngredientHandler} 
-              disabled={disabledInfo}
-              purchasable={this.state.purchasable}
-              price={this.state.totalPrice}/>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
+                ingredientAdded={this.addIngredientHandler}
+                ingredientRemoved={this.removeIngredientHandler} 
+                disabled={disabledInfo}
+                purchasable={this.state.purchasable}
+                price={this.state.totalPrice}/>
             </Aux>
         );
     }
